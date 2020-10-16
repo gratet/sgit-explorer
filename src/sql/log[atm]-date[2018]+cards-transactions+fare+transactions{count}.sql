a7d62da67ab2fa9cb442a641e37c4f86319eaf33
Benito M. Zaragozí <benizar@gmail.com>
2020-10-16 11:51:07 +0200

    Delete log[atm]-date[2018]+cards-transactions+fare+transactions{count}.sql

--- src/sql/log[atm]-date[2018]+cards-transactions+fare+transactions{count}.sql
+++
@@ -1,8 +0,0 @@
-
-SELECT f.name AS fare, 
-  Count(DISTINCT t.card) AS cards,
-  Count(t.id) AS transactions
-FROM transactions_atm t
-  INNER JOIN fares f ON t.fare_id=f.id
-GROUP BY f.name
-ORDER BY f.name ASC;
