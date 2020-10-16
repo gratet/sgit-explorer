1c5d2b40c491ca4e1ef6d8a46a2df6ae53cdc588
Benito M. Zaragozí <benizar@gmail.com>
2020-10-16 11:51:28 +0200

    Delete log[atm]-date[2018]+enabled_days~cards+units{2int}.sql

--- src/sql/log[atm]-date[2018]+enabled_days~cards+units{2int}.sql
+++
@@ -1,9 +0,0 @@
-WITH lifespan AS (
-select card,
-  CAST(julianday(max(date(time_stamp))) - julianday(min(date(time_stamp))) + 1 AS integer) as enabled_days
-from transactions_atm AS t
-group by card
-)
-SELECT DISTINCT enabled_days, Count(card) AS cards
-FROM lifespan
-GROUP BY enabled_days;
