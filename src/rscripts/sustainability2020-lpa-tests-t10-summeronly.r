34e44a593129803f4235b84fdcb98650a42cd6e7
benizar <benizar@gmail.com>
2021-08-02 18:57:35 +0200

    updated to bigquery

--- src/rscripts/sustainability2020-lpa-tests-t10-summeronly.r
+++ src/rscripts/sustainability2020-lpa-tests-t10-summeronly.r
@@ -7,141 +7,31 @@ library(stringr)
 library(ggcorrplot)
 library(sugrrants)
 library(tidyLPA)
-
-activity <- 
-  read_csv("dist/query_results/2018/log[atm]-date[2018]-card[summeronly]-fare[t10]+stops-municipalities-zones-routes-days-months+card+activity{count}.csv",
-           col_types = cols(
-             card = col_character(), 
-             active_days = col_integer(),
-             active_months = col_integer(),
-             used_routes = col_integer(),
-             visited_municipalities = col_integer(),
-             visited_stops = col_integer(),
-             visited_zones = col_integer()
-           )
-  )
-
-spatial_concentration <- 
-  read_csv("dist/query_results/2018/log[atm]-date[2018]-card[summeronly]-fare[t10]+transactions-transactions{tchains}-tarragonareus-cgc+card+transactions{count}.csv", 
-           col_types = cols(
-             card = col_character(),
-             transaction_chains_summer = col_integer(),
-             transactions_summer = col_integer(), 
-             transactions_cgc_summer = col_integer(),
-             transactions_tarragona_reus_summer = col_integer()
-           )
-  )
-
-stops_concentration <- 
-  read_csv("dist/query_results/2018/log[atm]-date[2018]-card[summeronly]-fare[t10]+stops-main2stops-main3stops+cards+transactions{topn}.csv", 
-           col_types = cols(
-             card = col_character(),
-             main_three_stops = col_integer(),
-             main_two_stops = col_integer(), 
-             stop_id_array = col_character(),
-             stop_transactions_array = col_character()
-           )
-  ) %>%
-  dplyr::select(-ends_with("array"))
-
-temporal_concentration <- 
-  read_csv("dist/query_results/2018/log[atm]-date[2018]-card[summeronly]-fare[t10]+total-weekend-weekend[hourly]-weekend[hourly[00,02,07,11,15,19,24]]-week-week[hourly]-week[hourly[00,02,07,11,15,19,24]]+card-fare+transactions{count}.csv",
-           col_types = cols(
-             card = col_character(),
-             fare = col_character(), transactions = col_integer(),
-             week = col_integer(), 
-             week_00 = col_integer(), week_01 = col_integer(), 
-             week_02 = col_integer(), week_03 = col_integer(), 
-             week_04 = col_integer(), week_05 = col_integer(), 
-             week_06 = col_integer(), week_07 = col_integer(), 
-             week_08 = col_integer(), week_09 = col_integer(), 
-             week_10 = col_integer(), week_11 = col_integer(), 
-             week_12 = col_integer(), week_13 = col_integer(), 
-             week_14 = col_integer(), week_15 = col_integer(), 
-             week_16 = col_integer(), week_17 = col_integer(), 
-             week_18 = col_integer(), week_19 = col_integer(), 
-             week_20 = col_integer(), week_21 = col_integer(), 
-             week_22 = col_integer(), week_23 = col_integer(), 
-             
-             weekend = col_integer(),
-             weekend_00 = col_integer(), weekend_01 = col_integer(), 
-             weekend_02 = col_integer(), weekend_03 = col_integer(),
-             weekend_04 = col_integer(), weekend_05 = col_integer(),
-             weekend_06 = col_integer(), weekend_07 = col_integer(), 
-             weekend_08 = col_integer(), weekend_09 = col_integer(),
-             weekend_10 = col_integer(), weekend_11 = col_integer(),
-             weekend_12 = col_integer(), weekend_13 = col_integer(), 
-             weekend_14 = col_integer(), weekend_15 = col_integer(),
-             weekend_16 = col_integer(), weekend_17 = col_integer(),
-             weekend_18 = col_integer(), weekend_19 = col_integer(), 
-             weekend_20 = col_integer(), weekend_21 = col_integer(),
-             weekend_22 = col_integer(), weekend_23 = col_integer(),
-             
-             week_00_02 = col_integer(), week_02_07 = col_integer(), 
-             week_07_11 = col_integer(), week_11_15 = col_integer(), 
-             week_15_19 = col_integer(), week_19_24 = col_integer(),
-             
-             weekend_00_02 = col_integer(), weekend_02_07 = col_integer(), 
-             weekend_07_11 = col_integer(), weekend_11_15 = col_integer(), 
-             weekend_15_19 = col_integer(), weekend_19_24 = col_integer()
-           )
-  )
-
-
-gaps_and_islands <- 
-  read_csv("dist/query_results/2018/log[atm]-date[2018]-card[summeronly]-fare[t10]+enabled+card+days{gaps.and.islands}.csv", 
-           col_types = cols(
-             card = col_character(),
-             enabled_days = col_integer()
-           )
-  )
-
-group_sizes<-
-  read_csv("dist/query_results/2018/log[atm]-date[2018]-card[summeronly]-fare[t10]+max_group_size-avg_group_size+card+transactions{gaps.and.islands}.csv", 
-           col_types = cols(
-             card = col_character(),
-             max_group_size = col_integer(),
-             avg_group_size = col_integer()
-           )
-  )
-
-
+library(cowplot)
 
 card_stats<-
-  activity %>% 
-  left_join(spatial_concentration,by = "card") %>% 
-  left_join(stops_concentration,by = "card") %>% 
-  left_join(temporal_concentration,by = "card")%>% 
-  left_join(gaps_and_islands,by = "card") %>% 
-  left_join(group_sizes,by="card")
+  read_csv("dist/bq-results-20210802-163739-7btj1k7n4qkp.csv", 
+           col_types = cols(active_days = col_integer(), 
+           active_months = col_integer(), active_period = col_integer(), 
+           avg_group_size = col_double(), card = col_character(), 
+           main_three_stops = col_integer(),
+           main_two_stops = col_integer(), max_group_size = col_integer(), 
+           min_group_size = col_integer(), transaction_chains = col_integer(), 
+           transactions = col_integer(), transactions_cgc = col_double(), 
+           transactions_tarragona_reus = col_double(), 
+           used_routes = col_integer(), visited_municipalities = col_integer(), 
+           visited_zones = col_integer(), weekdays = col_integer(), 
+           weekdays_rel = col_double(), weekends = col_integer(), 
+           weekends_rel = col_double())) %>% 
+  select(-transaction_chains,-weekdays,-weekends) %>%
+  mutate(weekdays=weekdays_rel,weekends=weekends_rel) %>% 
+  select(card,active_period, 
+         transactions, visited_municipalities, visited_zones, used_routes, active_days,active_months,
+         main_two_stops, main_three_stops,
+         transactions_tarragona_reus, transactions_cgc,
+         contains("group"),weekdays,weekends) 
 
 card_stats[is.na(card_stats)] <- 0
-
-# Keep main
-rm(list=setdiff(ls(), "card_stats"))
-
-# For Regex testing see: 
-# https://regexr.com/
-
-card_stats <- 
-  card_stats %>% 
-  mutate(transactions_tarragona_reus=transactions_tarragona_reus_summer) %>%
-  mutate(transactions_cgc=transactions_cgc_summer) %>% 
-  select(-fare,
-         -transactions_summer,
-         -transactions_tarragona_reus_summer,-transactions_cgc_summer,
-         -transaction_chains_summer) %>% 
-  select(card,enabled_days, 
-                transactions, visited_stops, visited_municipalities, visited_zones, used_routes, active_days,active_months,
-                main_two_stops, main_three_stops,
-                transactions_tarragona_reus, transactions_cgc,
-                contains("group")) 
-# %>%
-#   select(-matches("week(\\_\\d\\d){1}\\b")) %>%
-#   select(-matches("weekend(\\_\\d\\d){1}\\b"))
-#   select(-matches("week(\\_\\d\\d){2}\\b")) %>%
-#   select(-matches("weekend(\\_\\d\\d){2}\\b")) %>%
-  # select(-weekend_04)
 
 
 ## Quickly convert multiple columns into percentages
@@ -152,12 +42,17 @@ fast_percent<-
     }
   }
 
+# transactions_column_names<-
+#   card_stats %>%
+#   select(starts_with("week"),
+#          starts_with("main"),
+#          contains("reus"),
+#          contains("cgc")) %>%
+#   names()
+
 transactions_column_names<-
   card_stats %>%
-  select(starts_with("week"),
-         starts_with("main"),
-         contains("reus"),
-         contains("cgc")) %>%
+  select(starts_with("main")) %>%
   names()
 
 fast_percent(transactions_column_names,"transactions")
@@ -194,9 +89,9 @@ which(summary(model_subset)$adjr2 == max(summary(model_subset)$adjr2))
 #   x = n_predictors[which.max(summary(model_subset)$adjr2)],
 #   y = R_adjusted[which.max(summary(model_subset)$adjr2)]),
 #   colour = "red", size = 3)
-# p <- p +  scale_x_continuous(breaks = c(0:length(card_stats)-2)) + 
+# p <- p +  scale_x_continuous(breaks = c(0:length(card_stats)-2)) +
 #   theme_bw() +
-#   labs(title = "Best model subset for summer transactions", 
+#   labs(title = "Best model subset for summer transactions",
 #        x =  'Number of predictors',
 #        y = bquote(~R^2~"adjusted"))
 # p
@@ -278,22 +173,31 @@ write.csv(lpa_cluster_stats, file = "dist/lpa-cluster-stats.csv",row.names = FAL
 
 library(table1)
 
-table1(~ transactions + enabled_days + visited_stops 
-       + used_routes + active_days 
-       + main_three_stops 
-       + transactions_cgc + avg_group_size + max_group_size | class, 
+table1(~ transactions 
+       + visited_municipalities
+       + active_days + active_period
+       + max_group_size + avg_group_size + min_group_size
+       +weekdays + weekends | class, 
        data=lpa_clusters, overall="Total")
 
 
 ####################
 # Analyze clusters #
 ####################
+# t10_dataset <- 
+#   read_csv("dist/query_results/2018/log[atm]-date[2018]-card[summeronly]-fare[t10]+id-card-timestamp-agency-fare-route-stop-machine++transactions{list}.csv",
+#            col_types = cols(
+#              card = col_character()
+#            )) %>% 
+#   select(card,time_stamp)
+
+
 t10_dataset <- 
-  read_csv("dist/query_results/2018/log[atm]-date[2018]-card[summeronly]-fare[t10]+id-card-timestamp-agency-fare-route-stop-machine++transactions{list}.csv",
-           col_types = cols(
-             card = col_character()
-           )) %>% 
+  read_csv("dist/3consultes/bq-results-20210802-161347-k0ejdpk76naq.csv", 
+           col_types = cols(card = col_character()))%>% 
+  mutate(time_stamp=date_time) %>% 
   select(card,time_stamp)
+
 
 cl1_sample <-
   lpa_clusters %>% 
@@ -395,7 +299,8 @@ calview_detail_plot_df <-
   group_by(class,Date, Year,Mdate,Day,Time) %>% 
   count(class) %>% 
   ungroup() %>% 
-  rename("Transactions"=n)
+  rename("Transactions"=n)%>% 
+  drop_na()
 
 levels(calview_detail_plot_df$class) <- legend_labels
 
@@ -406,7 +311,7 @@ calview_detail_plot <-
   mutate(Week = if_else(Day %in% c("Saturday", "Sunday"), "Weekend", "Weekday")) %>%
   ggplot(aes(x = Time, y = Transactions, colour = Week)) +
   geom_line() + 
-  scale_y_continuous(breaks=c(0, 100, 200)) +
+  scale_y_continuous(breaks=c(0, 100, 200, 300)) +
   scale_x_continuous(breaks=c(0, 6, 12, 18)) +
   facet_calendar(~ Date) +
   geom_text(aes(label = format(Date, format = "%d")), colour = "black", x = Inf, y = Inf, hjust = 1.5, vjust = 1.5) +
@@ -418,7 +323,7 @@ calview_detail_plot <-
 calview_detail_plot<-
   ggdraw(calview_detail_plot) + 
   draw_label("June", x = 0.28, y = 0.95,size = 16) +
-  draw_label("July", x = 0.9, y = 0.95,size = 16) +
+  draw_label("July", x = 0.78, y = 0.58,size = 16) +
   draw_label("August", x = 0.14, y = 0.47,size = 16) +
   draw_label("September", x = 0.81, y = 0.47,size = 16)
 
@@ -426,7 +331,7 @@ ggsave(filename = "dist/img/tiff/activity-main-cluster-faceted-calendar.tiff",
        plot = calview_detail_plot, 
        height=20, width=30, units='cm', 
        dpi = 300)
-ggsave(filename = "dist/img/png/activity-main-cluster-faceted-calendar.png", 
+ggsave(filename = "dist/activity-main-cluster-faceted-calendar.png", 
        plot = calview_detail_plot, 
        height=20, width=30, units='cm', 
        dpi = 300)
